27a81eab8980d84294c66db4b336ca2c5c1b2927
Cameron <cdg341@gmail.com>
2022-06-06 01:35:34 -0500

    Complete logic for game

--- script.js
+++ script.js
@@ -1,28 +1,110 @@
-// gameBoard module
-let gameboardModule = (function () {
-  let gameBoard = [];
-  return { gameBoard };
-})();
+// Takes the information from gameBoard and outputs the X/O onto the screen
+let displayControllerModule = (() => {
+  const board = document.querySelectorAll(".grid-box");
 
-// displayControllerModule
-let displayControllerModule = (function () {
-  let display = () => {
-    console.log("This is a test");
+  const display = () => {
+    for (let i = 0; i < board.length; i++) {
+      board[i].textContent = gameBoardModule.getBoard(i);
+    }
   };
   return { display };
 })();
 
-// Set up player factory function
-let createPlayer = (playerName, playerNumber, assignment) => {
-  let getPlayerName = () => {
-    playerName;
-    console.log(`This is the name of player ${playerNumber}, ${playerName}, which is assigned O`);
+//Acts as the memory and gets written and read by logic and also calls on the display
+const gameBoardModule = (function () {
+  const gameBoard = ["", "", "", "", "", "", "", "", ""];
+  let message = "";
+
+  const setBoard = (index, sign) => {
+    gameBoard[index] = sign;
+    displayControllerModule.display();
   };
-  return { getPlayerName, playerName, playerNumber, assignment };
-};
 
-//Render contents of the gameBoard array to the page
-function render() {}
+  const setMessage = (messageFromLogic) => {
+    message = messageFromLogic;
+    displayControllerModule.display();
+  };
 
-let playerOne = createPlayer("Cameron", 1, "X");
-let playerTwo = createPlayer("Brandon", 2, "O");
+  const getBoard = (index) => {
+    return gameBoard[index];
+  };
+
+  const getMessage = () => {
+    return message;
+  };
+
+  const restartBoard = () => {
+    for (let i = 0; i < gameBoard.length; i++) {
+      gameBoard[i] = "";
+      displayControllerModule.display();
+    }
+  };
+
+  return { setBoard, setMessage, getBoard, getMessage, restartBoard };
+})();
+
+//Adds click events and outputs it to the gameBoard
+const logic = (() => {
+  let round = 1;
+  let gameStatus = true;
+
+  const playRound = (gridIndex) => {
+    if (gameStatus === false || gameBoardModule.getBoard(gridIndex) !== "") {
+      return;
+    }
+    gameBoardModule.setBoard(gridIndex, currentPlayer());
+    if (checkWinner(gridIndex)) {
+      gameBoardModule.setMessage(alert(`${currentPlayer()} WON!`));
+      gameStatus = false;
+      return;
+    }
+    if (round === 9) {
+      gameBoardModule.setMessage(alert("DRAW!"));
+      gameStatus = false;
+      return;
+    }
+    round++;
+  };
+
+  const checkWinner = (gridIndex) => {
+    const winCombos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    return winCombos
+      .filter((combinations) => combinations.includes(gridIndex))
+      .some((possibleCombos) => possibleCombos.every((index) => gameBoardModule.getBoard(index) == currentPlayer()));
+  };
+
+  const currentPlayer = () => {
+    return round % 2 === 1 ? "X" : "O";
+  };
+
+  const restartGame = () => {
+    round = 1;
+    gameStatus = true;
+    gameBoardModule.restartBoard();
+  };
+  return { restartGame, playRound };
+})();
+
+const eventHandler = (() => {
+  const board = document.querySelectorAll(".grid-box");
+  const restart = document.querySelector("button");
+
+  board.forEach((board) => {
+    board.addEventListener("click", (e) => {
+      logic.playRound(parseInt(e.target.dataset.order));
+    });
+
+    restart.addEventListener("click", () => {
+      logic.restartGame();
+    });
+  });
+})();
